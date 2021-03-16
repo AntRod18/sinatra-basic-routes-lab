f6b1866173563779a78a3272bdd6a04cef6fa722
AntRod18 <antrod1895@gmail.com>
2021-03-16 12:12:20 -0400

    commit tree cb3297f4205cdfa2e4521c785d158b424a5f929b

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Anthony"
+    end
+
+    get '/hometown' do 
+        "My hometown is Jersey City"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is La noche de anoche"
+    end
 end
